be3287bfb382c7a3d98fe2307256eba7442cb0de
Robert Pieter van Leeuwen <luivatra@gmail.com>
2024-01-07 13:59:45 +0100

    fix if structure

--- src/main/scala/im/paideia/governance/contracts/ProtoDAO.scala
+++ src/main/scala/im/paideia/governance/contracts/ProtoDAO.scala
@@ -160,35 +160,37 @@ class ProtoDAO(contractSignature: PaideiaContractSignature)
               )
             )
           PaideiaEventResponse(1)
-        } else if (
-          te.tx.getInputs().size() > 4 && getUtxoSet.contains(
-            te.tx.getInputs().get(0).getBoxId()
-          )
-        ) {
-          val protoDAOInput = te.tx.getInputs().get(0)
-          val protoDAOBox =
-            ProtoDAOBox.fromInputBox(te.ctx, boxes(protoDAOInput.getBoxId()))
-          Paideia
-            .getConfig(protoDAOBox.dao.key)
-            .handleUpdateEvent(
-              UpdateConfigEvent(
-                te.ctx,
-                protoDAOBox.dao.key,
-                if (te.mempool)
-                  Left(
-                    protoDAOBox.digestOpt.get
-                  )
-                else
-                  Right(te.height),
-                Array[DAOConfigKey](),
-                Array[(DAOConfigKey, Array[Byte])](),
-                CreateDAO(PaideiaContractSignature(daoKey = Env.paideiaDaoKey))
-                  .getInsertOperations(protoDAOBox.dao)
-              )
-            )
-          PaideiaEventResponse(1)
         } else {
-          PaideiaEventResponse(0)
+          if (
+            te.tx.getInputs().size() > 4 && getUtxoSet.contains(
+              te.tx.getInputs().get(0).getBoxId()
+            )
+          ) {
+            val protoDAOInput = te.tx.getInputs().get(0)
+            val protoDAOBox =
+              ProtoDAOBox.fromInputBox(te.ctx, boxes(protoDAOInput.getBoxId()))
+            Paideia
+              .getConfig(protoDAOBox.dao.key)
+              .handleUpdateEvent(
+                UpdateConfigEvent(
+                  te.ctx,
+                  protoDAOBox.dao.key,
+                  if (te.mempool)
+                    Left(
+                      protoDAOBox.digestOpt.get
+                    )
+                  else
+                    Right(te.height),
+                  Array[DAOConfigKey](),
+                  Array[(DAOConfigKey, Array[Byte])](),
+                  CreateDAO(PaideiaContractSignature(daoKey = Env.paideiaDaoKey))
+                    .getInsertOperations(protoDAOBox.dao)
+                )
+              )
+            PaideiaEventResponse(1)
+          } else {
+            PaideiaEventResponse(0)
+          }
         }
 
       }
